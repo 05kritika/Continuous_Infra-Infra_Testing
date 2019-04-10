29b015d4ee233d70990c2f83939379635aee133f
05kritika <39408273+05kritika@users.noreply.github.com>
2019-04-10 19:23:47 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
+## VARIABLES.TF
+------------------
+
+# Two-Tier example from https://github.com/terraform-providers/terraform-provider-aws
+
 variable "private_key_path" {
   description = <<DESCRIPTION
 Path to the SSH public key to be used for authentication.
@@ -6,10 +11,13 @@ connect.
 
 Example: ~/.ssh/terraform.pub
 DESCRIPTION
+
+  default ="/home/ubuntu/kritika.pem"
 }
 
 variable "key_name" {
   description = "Desired name of AWS key pair"
+  default = "kritika"
 }
 
 variable "aws_region" {
@@ -17,13 +25,13 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-# Ubuntu Precise 12.04 LTS (x64)
-variable "aws_amis" {
-  default = {
-    eu-central-1 = "ami-fa2fb595"
-    eu-west-1 = "ami-674cbc1e"
-    us-east-1 = "ami-1d4e7a66"
-    us-west-1 = "ami-969ab1f6"
-    us-west-2 = "ami-8803e0f0"
-  }
+variable "instance_type" {
+  default = "t2.micro"
 }
+
+# Ubuntu Precise 18.04 LTS (x64)
+variable "aws_amis" {
+  description = "ubuntu 18.04 AMI"
+  default = "ami-0a313d6098716f372"
+}
+
