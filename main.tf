f220169d73cb9311f0e21cdc96fe0b7f19673f6e
05kritika <39408273+05kritika@users.noreply.github.com>
2019-04-10 19:22:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,31 @@
-# Two-Tier example from https://github.com/terraform-providers/terraform-provider-aws
+# DATA
+#data "aws_region" "current" {}
+data "aws_availability_zones" "available" {}
 
 # Specify the provider and access details
 provider "aws" {
   region = "${var.aws_region}"
 }
 
+resource "aws_vpc_dhcp_options" "DHCP" {
+  domain_name_servers = ["AmazonProvidedDNS"]
+}
+
 # Create a VPC to launch our instances into
 resource "aws_vpc" "default" {
   cidr_block = "10.0.0.0/16"
+  instance_tenancy = "default"
+  enable_dns_hostnames = "true"
+}
+
+resource "aws_vpc_dhcp_options_association" "dns_resolver" {
+  vpc_id = "${aws_vpc.default.id}"
+  dhcp_options_id = "${aws_vpc_dhcp_options.DHCP.id}"
 }
 
 # Create an internet gateway to give our subnet access to the outside world
 resource "aws_internet_gateway" "default" {
   vpc_id = "${aws_vpc.default.id}"
-}
-
-# Grant the VPC internet access on its main route table
-resource "aws_route" "internet_access" {
-  route_table_id         = "${aws_vpc.default.main_route_table_id}"
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.default.id}"
 }
 
 # Create a subnet to launch our instances into
@@ -28,6 +34,24 @@ resource "aws_subnet" "default" {
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
 }
+
+resource "aws_route_table" "default" {
+  vpc_id = "${aws_vpc.default.id}"
+}
+
+resource "aws_route_table_association" "default_association" {
+  subnet_id = "${aws_subnet.default.id}"
+  route_table_id = "${aws_route_table.default.id}"
+#  vpc_id = "${aws_vpc.default.id}"
+}
+
+# Grant the VPC internet access on its main route table
+resource "aws_route" "internet_access" {
+  route_table_id         = "${aws_route_table.default.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.default.id}"
+}
+
 
 # A security group for the ELB so it is accessible via the web
 resource "aws_security_group" "elb" {
@@ -41,7 +65,8 @@ resource "aws_security_group" "elb" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
+ }
+
   # outbound internet access
   egress {
     from_port   = 0
@@ -91,7 +116,7 @@ resource "aws_elb" "web" {
   instances       = ["${aws_instance.web.id}"]
 
   listener {
-    instance_port     = 80
+   instance_port     = 80
     instance_protocol = "http"
     lb_port           = 80
     lb_protocol       = "http"
@@ -102,21 +127,22 @@ resource "aws_elb" "web" {
 resource "aws_instance" "web" {
 
   key_name = "${var.key_name}"
-  # The connection block tells our provisioner how to
-  # communicate with the resource (instance)
+  # The connection block tells our provisioner how to communicate with the resource (instance)
   connection {
-    # The default username for our AMI
+  # The default username for our AMI
+  # host = "${self.public_ip}"
+    host = "${aws_instance.web.public_dns}"
+  # type = "ssh"
     user = "ubuntu"
     private_key = "${file(var.private_key_path)}"
 
     # The connection will use the local SSH agent for authentication.
   }
 
-  instance_type = "t2.micro"
+  instance_type = "${var.instance_type}"
 
-  # Lookup the correct AMI based on the region
-  # we specified
-  ami = "${lookup(var.aws_amis, var.aws_region)}"
+  # Lookup the correct AMI based on the region we specified
+  ami = "${var.aws_amis}"
 
   # The name of our SSH keypair we created above.
   # key_name = "${aws_key_pair.auth.id}"
@@ -124,19 +150,16 @@ resource "aws_instance" "web" {
   # Our Security group to allow HTTP and SSH access
   vpc_security_group_ids = ["${aws_security_group.default.id}"]
 
-  # We're going to launch into the same subnet as our ELB. In a production
-  # environment it's more common to have a separate private subnet for
-  # backend instances.
+  # We're going to launch into the same subnet as our ELB. In a production environment its more common to have a separate private subnet for backend instances.
   subnet_id = "${aws_subnet.default.id}"
 
-  # We run a remote provisioner on the instance after creating it.
-  # In this case, we just install nginx and start it. By default,
-  # this should be on port 80
+  # We run a remote provisioner on the instance after creating it. In this case, we just install nginx and start it. By default, this should be on port 80
 #  provisioner "remote-exec" {
 #    inline = [
+#      "sleep 20",
 #      "sudo apt-get -y update",
 #      "sudo apt-get -y install nginx",
-#      "sudo systemctl start nginx",
+#      "sudo systemctl start nginx"
 #    ]
 #  }
 }
